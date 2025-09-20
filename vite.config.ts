5ec58cf703b992a70319db0f0c6627f5d54e6389
yoosunho1 <sunho.yoo@samsung.com>
2025-09-20 10:36:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig({
             output: {
                 manualChunks: undefined,
                 assetFileNames: (assetInfo) => {
+                    if (!assetInfo.name) {
+                        return 'assets/[name]-[hash][extname]';
+                    }
                     const info = assetInfo.name.split('.');
                     const ext = info[info.length - 1];
                     if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
